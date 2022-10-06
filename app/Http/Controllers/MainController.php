2a2e9399b4ac0725cb2b79402172c8853fe5ad60
suguru-gbgg <takagisn12@gmail.com>
2022-10-06 15:30:33 +0900

    c

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,21 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\text;
-use App\Models\YourModel;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Text;
+
 
 class MainController extends Controller
 {
     function insert(Request $request){
-      for($i=1; $i<=$request->textcount; $i++){
+      for($i=0; $i<=$request->list; $i++){
         $a = "htext".$i;
-        $task = new YourModel();
+        $task = new Text();
 
         $task->user_id = Auth::user()->id;
-        $task->text = $request->a;
+        $task->text = $request->$a;
 
         $task->save();
       }
     }
+}
+
+class MainController extends Controller
+{
+  function find(){
+    $text = Text::find(Auth::user()->id);
+    echo $text->text;
+  }
 }
--- database/migrations/2022_10_02_235200_create_texts_table.php
+++ database/migrations/2022_10_02_235200_create_texts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('texts', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('text');
             $table->timestamps();
         });
     }
