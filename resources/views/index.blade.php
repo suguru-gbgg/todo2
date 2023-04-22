debf63d8ea88a153c392de15498be7d22390d8de
suguru-gbgg <takagisn12@gmail.com>
2023-04-22 23:34:01 +0900

    inputのtextをhiddenにした

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,18 @@
 <button type="submit" class="addtext">追加</button>
 <form action="MainController" method="post">
 @csrf
-@foreach($texts as $text)
-<ul class="parent">
-    <li class="child">
-        {{ $text->text }}
-        <button type="button" class="delbtn">削除</button>
-    </li>
-</ul>
-@endforeach
+<?php $list = 0; ?>
+    <ul class="parent">
+        @foreach($texts as $text)
+            <li class="child">
+                {{ $text->text }}
+                <input type="hidden" value="{{ $text->text }}" name="htext{{ $list }}" class="name">
+                <input type="hidden" value="{{ $list }}" class="list" name='list'>
+                <button type="button" class="delbtn">削除</button>
+            </li>
+            <?php $list++ ?>
+        @endforeach
+    </ul>
     <button type="submit">保存</button>
 </form>
 
